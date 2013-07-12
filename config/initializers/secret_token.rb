9bdbf44d8d3246492a2a498c19fb17155790a748
André Barbosa <albmail88@gmail.com>
2013-07-12 21:40:20 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OrderManagementSystem::Application.config.secret_key_base = '90f8ccdbf8fca2ed3b6bac35654a06434bc090d4d758bda398e927d860b8c6f7b64096f769ef658454870db2fcec2c4daea1c3818039d20b4328331bc5f8e060'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+OrderManagementSystem::Application.config.session_store :cookie_store, key: '_OrderManagementSystem_session'
