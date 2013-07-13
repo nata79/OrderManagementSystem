e9391895f2641e6768752dc297908ccd0a6b4a1c
André Barbosa <albmail88@gmail.com>
2013-07-13 12:11:01 +0100

    bug fix on product spec

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,13 +2,13 @@ require 'spec_helper'
 
 describe Product do
   include MoneyRails::TestHelpers
-  
+
   it 'requires a name' do
     build(:product, name: nil).should_not be_valid
   end
 
   it 'has a net_price attribute with the type Money' do
-    monetize(:net_price_pennies).as(:net_price).should be_true
+    build(:product).should monetize(:net_price_pennies).as(:net_price)
   end
 
   it 'requires a net price higher than 0' do
